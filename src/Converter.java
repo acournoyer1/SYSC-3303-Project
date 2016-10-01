6563210e20f7960774b4964f40e814a88dda2cbb
DanMihailescu <as42554525@yahoo.ca>
2016-10-01 16:49:33 -0400

    Added Documentation

--- src/Converter.java
+++ src/Converter.java
@@ -1,14 +1,22 @@
-
+/**
+ * Creates a converter that turn a byte array into a string which it returns
+ * 
+ * Team 11  
+ * V1.16
+ */
 public class Converter {
 
+	//Converts byte array into a string
 	public static String convertMessage(byte[] message)
 	{
+		//Initiates string with type of request
 		String byteString = "[" + message[0] + ", " + message[1];
 		String s = "[" + message[0] + ", " + message[1];
 		
 		int startIndex = 2;
 		int endIndex = 0;
 		int zeroCount = 0;
+		//Keeps running until it gets two zero's in a row
 		for(int i = 2; zeroCount < 2; i++)
 		{
 			byteString += ", " + message[i];
@@ -16,16 +24,19 @@ public class Converter {
 			{
 				zeroCount++;
 				endIndex = i;
+				//Creates byte array containing bytes for filename or mode
 				byte[] str = new byte[endIndex - startIndex];
 				int k = 0;
 				for(int j = startIndex; j < endIndex && k < str.length; j++, k++)
 				{
 					str[k] = message[j];
 				}
+				//Turns byte array into a string
 				s += ", " + new String(str) + ", 0";
 				startIndex = i+1;
 			}
 		}
+		//Ends string and returns it
 		byteString += "]";
 		s += "] " + byteString; 
 		
